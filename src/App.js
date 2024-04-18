fb400f703e8ec66df2a95bb41e6ff4e9807bb1cf
Richard Donald <richard_donald92@outlook.com>
2024-04-18 19:25:43 -0400

    create modules

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+
+    return <textarea autoFocus maxLength={5} style={{border: '1px solid red'}} spellCheck/>
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 // 1) Import the React and ReactDOM libraries
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App'
 
 // 2) Get a reference to the div with ID root
 const el = document.getElementById('root');
 
 // 3) Tell React to take control of that element
 const root = ReactDOM.createRoot(el);
-// 4) Create a component
-function App() {
-    return <h1>Hello World</h1>;
-}
-// 5) Show the component on the screen
-root.render(<App />);
+
+// 4) Show the component on the screen
+
+
+root.render(<App/>);
